2134a2d8681b23b5d4a4f97934f0f3861f0efb78
이승후 <ron222@naver.com>
2022-07-06 11:43:36 +0900

    add: LedBoard project

---
+++ LedBoard/LedBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LedBoard
+//
+//  Created by 이승후 on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
