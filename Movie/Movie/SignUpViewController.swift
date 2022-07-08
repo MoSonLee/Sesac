5848cf12213bd47a111285b99c1eb7cc36f82a8e
이승후 <ron222@naver.com>
2022-07-08 20:08:48 +0900

    refactor: refactor code

--- Movie/Movie/SignUpViewController.swift
+++ Movie/Movie/SignUpViewController.swift
@@ -9,84 +9,60 @@ import UIKit
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var IdTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var nicknameTextField: UITextField!
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var recommendCodeTextField: UITextField!
-    @IBOutlet weak var SignUpUIButton: UIButton!
+    @IBOutlet var textFieldArray: [UITextField]!
+    @IBOutlet weak var signUpUIButton: UIButton!
     @IBOutlet weak var checkSwitch: UISwitch!
+    
+    let placeholderArray = ["이메일 주소 또는 전화번호", "비밀번호", "닉네임", "위치", "추천코드"]
+    let keybordTypeArray: [UIKeyboardType] = [.emailAddress, .default, .alphabet, .asciiCapable, .numberPad]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        recommendCodeTextField.delegate = self;
+        for index in 0..<textFieldArray.count {
+            textFieldArray[index].attributedPlaceholder = NSAttributedString(string: placeholderArray[index], attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+            textFieldArray[index].textAlignment = .center
+            textFieldArray[index].backgroundColor = UIColor.darkGray
+            textFieldArray[index].keyboardType = keybordTypeArray[index]
+            textFieldArray[index].delegate = self
+        }
+        textFieldArray[1].isSecureTextEntry = true
         
-        IdTextField.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
-        IdTextField.textAlignment = .center
+        signUpUIButton.setTitle("회원가입", for: .normal)
+        signUpUIButton.tintColor = .black
+        signUpUIButton.backgroundColor = .white
+        signUpUIButton.layer.cornerRadius = 8
+        signUpUIButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
-        passwordTextField.textAlignment = .center
-        
-        nicknameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
-        nicknameTextField.textAlignment = .center
-        
-        locationTextField.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
-        locationTextField.textAlignment = .center
-        
-        recommendCodeTextField.attributedPlaceholder = NSAttributedString(string: "추천 코드", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
-        recommendCodeTextField.textAlignment = .center
-        
-        SignUpUIButton.setTitle("회원가입", for: .normal)
-        SignUpUIButton.tintColor = .black
-        SignUpUIButton.backgroundColor = .white
-        SignUpUIButton.layer.cornerRadius = 8
-        SignUpUIButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        
-        checkSwitch.setOn(false, animated: true)
+        checkSwitch.setOn(true, animated: true)
         checkSwitch.onTintColor = .red
         checkSwitch.thumbTintColor = .lightGray
-        
-        IdTextField.backgroundColor = UIColor.lightGray
-        passwordTextField.backgroundColor = UIColor.lightGray
-        nicknameTextField.backgroundColor = UIColor.lightGray
-        locationTextField.backgroundColor = UIColor.lightGray
-        recommendCodeTextField.backgroundColor = UIColor.lightGray
-        
-        IdTextField.keyboardType = .emailAddress
-        passwordTextField.keyboardType = .default
-        //        passwordTextField.isSecureTextEntry = true
-        nicknameTextField.keyboardType = .alphabet
-        locationTextField.keyboardType = .asciiCapable
-        recommendCodeTextField.keyboardType = .numberPad
     }
-    
-    // textfiled에 숫자만 입력되게 하는 로직
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
-            return false
-        }
-        return true
-    }
-    
-    // ID와 비밀번호 둘 다 입력을 해야 회원가입 성공이 뜨는 로직
-    @IBAction func signupbutton(_ sender: Any) {
+    // ID와 비밀번호 둘 다 입력 + 비밀번호 6자 이하 제한해서 회원가입 성공이 뜨는 로직
+    @IBAction func signupbuttonTapped(_ sender: Any) {
         view.endEditing(true)
-        if IdTextField.text!.count == 0 || passwordTextField.text!.count < 6 {
-            SignUpUIButton.setTitle("회원가입 실패", for: .highlighted)
+        if textFieldArray[0].text!.count == 0 || textFieldArray[1].text!.count < 6 {
+            signUpUIButton.setTitle("회원가입 실패", for: .highlighted)
         } else {
-            SignUpUIButton.setTitle("회원가입 성공", for: .highlighted)
+            signUpUIButton.setTitle("회원가입 성공", for: .highlighted)
         }
     }
-    
+    //background tap 했을 때 키보드 내려가는 로직
     @IBAction func tapBackGroundGesture(_ sender: Any) {
         view.endEditing(true)
     }
+    //키보드에서 done or return을 누르면 꺼지는 로직
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
     }
-    //키보드에서 done or return을 누르면 꺼지는 로직
-    @IBAction func done2(_ sender: UITextField) {
-        print("exit")
+    // 추천코드입력 textField에 숫자만 입력되게 하는 로직
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.tag == 4 {
+            guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string))
+            else {
+                return false
+            }
+        }
+        return true
     }
 }
