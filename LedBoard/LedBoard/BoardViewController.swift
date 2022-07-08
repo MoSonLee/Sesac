5ae17ffd1330501f95343e77d113787d7f91197d
이승후 <ron222@naver.com>
2022-07-08 20:26:34 +0900

    feat: change color logic

--- LedBoard/LedBoard/BoardViewController.swift
+++ LedBoard/LedBoard/BoardViewController.swift
@@ -7,73 +7,44 @@
 
 import UIKit
 
-class BoardViewController: UIViewController {
+final class BoardViewController: UIViewController {
     
-    @IBOutlet weak var userTextField: UITextField!
-    @IBOutlet weak var sendButton: UIButton!
-    @IBOutlet weak var changecolorButton: UIButton!
-    @IBOutlet weak var boardLabel: UILabel!
-    @IBOutlet weak var toggleView: UIView!
+    @IBOutlet private weak var userTextField: UITextField!
+    @IBOutlet private weak var sendButton: UIButton!
+    @IBOutlet private weak var changecolorButton: UIButton!
+    @IBOutlet private weak var boardLabel: UILabel!
+    @IBOutlet private weak var toggleView: UIView!
     
-    @IBOutlet var buttonList: [UIButton]!
+    @IBOutlet private var buttonList: [UIButton]!
+    let labelColor: [UIColor] = [.yellow, .darkGray, .red, .blue, .white]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         designTextField()
-        designButton(sendButton, buttonTitle: "전송",  HighlightedTitle: "빨리 보내", bgColor: .lightGray)
-        designButton(changecolorButton, buttonTitle: "색상 변경", HighlightedTitle: "빨리 변경해", bgColor: .lightGray)
+        configureButton(sendButton, buttonTitle: "전송",  HighlightedTitle: "빨리 보내", bgColor: .lightGray)
+        configureButton(changecolorButton, buttonTitle: "색상 변경", HighlightedTitle: "빨리 변경해", bgColor: .lightGray)
         
-        boardLabel.numberOfLines = 0
+        boardLabel.numberOfLines = 2
         boardLabel.backgroundColor = .clear
         
         userTextField.placeholder = "내용을 작성해주세요"
         userTextField.font = .boldSystemFont(ofSize: 20)
         userTextField.keyboardType = .emailAddress
-        
-        // 1. 반복문
-        //        let buttonArray: [UIButton] = [sendButton, changecolorButton]
-        //
-        //        for item in buttonArray {
-        //            item.backgroundColor = .yellow
-        //            item.layer.cornerRadius = 2
-        //        }
-        
-        //아웃렛 컬렉션
-        //        for item in buttonList {
-        //            item.backgroundColor = .blue
-        //            item.layer.cornerRadius = 2
-        //        }
     }
     
-    func designTextField() {
-        userTextField.placeholder = "내용을 작성해주세요"
-        userTextField.keyboardType = .emailAddress
-        userTextField.font = .boldSystemFont(ofSize: 20)
-    }
-    
-    func designButton(_ buttonName: UIButton, buttonTitle: String, HighlightedTitle: String, bgColor: UIColor) {
-        buttonName.setTitle(buttonTitle, for: .normal)
-        buttonName.setTitle(HighlightedTitle, for: .highlighted)
-        buttonName.layer.cornerRadius = 8
-        buttonName.layer.borderColor = UIColor.black.cgColor
-        buttonName.layer.borderWidth = 3
-        buttonName.setTitleColor(.black, for: .normal)
-        buttonName.setTitleColor(.red, for: .highlighted)
-    }
-    
-    @IBAction func confirmButtonTapped(_ sender: UIButton) {
+    @IBAction private func confirmButtonTapped(_ sender: UIButton) {
         boardLabel.text = userTextField.text
         view.endEditing(true)
     }
     
-    @IBAction func changeColorButtonTapped(_ sender: Any) {
+    @IBAction private func changeColorButtonTapped(_ sender: Any) {
     }
     
-    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+    @IBAction private func tapGestureClicked(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
     
-    @IBAction func viewHidden(_ sender: Any) {
+    @IBAction private func viewHidden(_ sender: Any) {
         if toggleView.isHidden {
             toggleView.isHidden = false
         } else {
@@ -81,7 +52,24 @@ class BoardViewController: UIViewController {
         }
     }
     
-    @IBAction func keybordDismiss(_ sender: Any) {
-        
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        boardLabel.textColor = labelColor.randomElement()
+    }
+    @IBAction private func keybordDismiss(_ sender: Any) {}
+    
+    private func designTextField() {
+        userTextField.placeholder = "내용을 작성해주세요"
+        userTextField.keyboardType = .emailAddress
+        userTextField.font = .boldSystemFont(ofSize: 20)
+    }
+    
+    private func configureButton(_ buttonName: UIButton, buttonTitle: String, HighlightedTitle: String, bgColor: UIColor) {
+        buttonName.setTitle(buttonTitle, for: .normal)
+        buttonName.setTitle(HighlightedTitle, for: .highlighted)
+        buttonName.layer.cornerRadius = 8
+        buttonName.layer.borderColor = UIColor.black.cgColor
+        buttonName.layer.borderWidth = 3
+        buttonName.setTitleColor(.black, for: .normal)
+        buttonName.setTitleColor(.red, for: .highlighted)
     }
 }
