4e434bfcac187eddb34dd739c86e17844a1aa2fc
이승후 <ron222@naver.com>
2022-07-12 05:25:57 +0900

    feat: initializer button + count logic complete

--- SesacWeek2/SesacWeek2/ViewController.swift
+++ SesacWeek2/SesacWeek2/ViewController.swift
@@ -7,24 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var labelArray: [UILabel]!
-    @IBOutlet var buttonArray: [UIButton]!
+final class ViewController: UIViewController {
     
-    var imageArray: [UIImage] = []
-    var labelName: [String] = ["행복해", "사랑해", "좋아해", "당황해 ", "속상해", "우울해", "심심해", "행복해", "행복해"]
-    var buttonTtitle: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    var i = 0
-    var j = 0
-    var a = 0
-    var plus = 0
+    @IBOutlet private var labelArray: [UILabel]!
+    @IBOutlet private var buttonArray: [UIButton]!
+    
+    private var imageArray: [UIImage] = []
+    private var labelName: [String] = ["행복해", "사랑해", "좋아해", "당황해 ", "속상해", "우울해", "심심해", "행복해", "행복해"]
+    private var labelCount: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    private var i = 0
+    private var j = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         buttonArray.forEach {
             $0.setImage(UIImage(named: "sesac_slime\(i + 1)")?.withRenderingMode(.alwaysOriginal), for: .normal)
-            $0.setTitle(buttonTtitle[i], for: .normal)
             i += 1
         }
         
@@ -36,12 +34,28 @@ class ViewController: UIViewController {
         }
     }
     
-    //난 행복해만 올라갈래..
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        plus += 1
-        var a = 0
-        labelArray[a].text = labelName[a] + String(plus)
-        a+=1
+    @IBAction private func buttonTapped(_ sender: UIButton) {
+        for index in sender.tag..<9 {
+            switch sender.tag {
+            case labelArray[index].tag:
+                button(sender, labelArray[index])
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction private func initializationButtonTapped(_ sender: UIButton) {
+        for index in 0..<labelName.count {
+            labelArray[index].text = labelName[index]
+            labelCount[index] = 0
+        }
+    }
+    
+    private func button(_ sender: UIButton, _ sender2: UILabel)  {
+        labelCount[sender2.tag] += 1
+        if sender.tag == sender2.tag {
+            sender2.text = labelName[sender2.tag] + String(labelCount[sender2.tag])
+        }
     }
 }
-
