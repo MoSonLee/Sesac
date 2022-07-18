3242a3867cefec41d12ff50787a6005728846804
이승후 <ron222@naver.com>
2022-07-18 22:49:41 +0900

    feat + refactor: change alert logic to extension + refactor
    use extension + handler(when ok tapped -> set date, day label, cancel tapped -> nil)

--- AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
+++ AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
@@ -16,52 +16,52 @@ final class ViewController: UIViewController {
     @IBOutlet weak var initializerButton: UIButton!
     
     private var index = 0
-    private var userIndex = 0
     private let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         checkiOSVersion()
         setUserDefaults()
+        setIndexUserDefaults()
         super.viewDidLoad()
         setButton()
         setDateLabel()
         setDayLabel()
         setImage()
     }
-    //datePicker 값이 변했을 때 일어나는 로직
+    
     @IBAction private func datePickerValueChanged(_ sender: UIDatePicker) {
-        //DateForamtter: 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss)
-        let format = DateFormatter()
-        format.dateFormat = "yy년 M월 d일"
-        imageCollection[index].tag = index
-        dateLabelCollection[index].tag = index
-        if imageCollection[index].tag == dateLabelCollection[index].tag {
-            dateLabelCollection[index].text = dDayDate()
-            dayLabelCollection[index].text = format.string(from: datePicker.date)
-            userDefaults.set(dateLabelCollection[index].text, forKey: "date\(index)")
-            userDefaults.set(dayLabelCollection[index].text, forKey: "day\(index)")
-            showAlert("날짜 변경 완효")
-            index += 1
-            index == 4 ? index = 0 : nil
-        }
+        showAlert("날짜를 변경하시겠습니까?")
     }
     
     @IBAction func InitializerButtonTapped(_ sender: UIButton) {
         for index in 0..<dayLabelCollection.count {
             dayLabelCollection[index].text = ""
             dateLabelCollection[index].text = ""
-            showAlert("초기화 완료")
+            showInitializerAlert("초기화 완료")
             userDefaults.set("", forKey: "date\(index)")
             userDefaults.set("", forKey: "day\(index)")
         }
+        index = 0
+        userDefaults.set(index, forKey: "indexKey")
     }
     
-    //alert창 띄워주는 로직
-    private func showAlert(_ alertText: String) {
-        let alert = UIAlertController(title: alertText, message: nil, preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style:.destructive, handler: nil)
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
+    private func checkiOSVersion() {
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        } else {
+            datePicker.preferredDatePickerStyle = .wheels
+        }
+    }
+    
+    private func setUserDefaults() {
+        for count in 0..<dateLabelCollection.count {
+            dateLabelCollection[count].text = userDefaults.string(forKey: "date\(count)")
+            dayLabelCollection[count].text = userDefaults.string(forKey: "day\(count)")
+        }
+    }
+    
+    private func setIndexUserDefaults() {
+        index = userDefaults.integer(forKey: "indexKey")
     }
     
     //dDay Date 구하는 로직 -> 조금 더 효율적이게 작성할 수 없을까?
@@ -72,28 +72,7 @@ final class ViewController: UIViewController {
         let selectedDate = calendar.startOfDay(for: datePicker.date)
         let dDayDate = calendar.dateComponents([.day], from: firstDate, to: selectedDate)
         dDayDateStringFormed =  dDayDate.day! > 0 ? "D + \(String(dDayDate.day!))" :"D \(String(dDayDate.day!))"
-        if dDayDate.day! % 100 == 0 {
-            let alert = UIAlertController(title: "\(String(describing: dDayDate.day!))일입니다!", message: "축하드립니다!", preferredStyle: .alert)
-            let ok = UIAlertAction(title: "확인", style:.destructive, handler: nil)
-            alert.addAction(ok)
-            present(alert, animated: true, completion: nil)
-        }
         return dDayDateStringFormed
-    }
-    
-    private func setUserDefaults() {
-        for count in 0..<dateLabelCollection.count {
-            dateLabelCollection[count].text = userDefaults.string(forKey: "date\(count)")
-            dayLabelCollection[count].text = userDefaults.string(forKey: "day\(count)")
-        }
-    }
-    //iOS version에 따라 datePicker 스타일을 다르게 해줌
-    private func checkiOSVersion() {
-        if #available(iOS 14.0, *) {
-            datePicker.preferredDatePickerStyle = .inline
-        } else {
-            datePicker.preferredDatePickerStyle = .wheels
-        }
     }
     
     private func setButton() {
@@ -108,8 +87,7 @@ final class ViewController: UIViewController {
         dateLabelCollection.forEach {
             $0.numberOfLines = 1
             $0.textColor = .white
-            $0.font = .preferredFont(forTextStyle: .title1)
-            $0.font = .boldSystemFont(ofSize: 30)
+            $0.font = .systemFont(ofSize: 40, weight: .bold)
         }
     }
     
@@ -117,8 +95,7 @@ final class ViewController: UIViewController {
         dayLabelCollection.forEach {
             $0.numberOfLines = 2
             $0.textColor = .white
-            $0.font = .preferredFont(forTextStyle: .body)
-            $0.font = .systemFont(ofSize: 20)
+            $0.font = .systemFont(ofSize: 20, weight: .bold)
             $0.textAlignment = .center
         }
     }
@@ -132,5 +109,55 @@ final class ViewController: UIViewController {
             imageCollection[i].layer.cornerRadius = imageCollection[i].frame.height/2 - 50
         }
     }
+    
+    private func datePickerChanged() {
+        let format = DateFormatter()
+        format.dateFormat = "yy년 M월 d일"
+        imageCollection[index].tag = index
+        dateLabelCollection[index].tag = index
+        if imageCollection[index].tag == dateLabelCollection[index].tag {
+            dateLabelCollection[index].text = dDayDate()
+            dayLabelCollection[index].text = format.string(from: datePicker.date)
+            userDefaults.set(dateLabelCollection[index].text, forKey: "date\(index)")
+            userDefaults.set(dayLabelCollection[index].text, forKey: "day\(index)")
+            index += 1
+            index == 4 ? index = 0 : nil
+        }
+        userDefaults.set(index, forKey: "indexKey")
+    }
 }
 
+extension ViewController {
+    func showAlert(_ alertText: String) {
+        let alert = UIAlertController(title: alertText, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(
+            title: "OK",
+            style:.destructive,
+            handler: {
+                (alert: UIAlertAction!) in
+                self.datePickerChanged()
+                self.dismiss(animated: true, completion: nil)
+            })
+        let cancel = UIAlertAction(
+            title: "cancel",
+            style: .destructive,
+            handler: nil
+        )
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController {
+    func showInitializerAlert(_ alertText: String) {
+        let alert = UIAlertController(title: alertText, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(
+            title: "OK",
+            style: .destructive,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
