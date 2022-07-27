ef33da45807dce467a1e12866af2fd9bcd34524e
이승후 <ron222@naver.com>
2022-07-27 12:46:24 +0900

    add: playground

--- Playground/MyPlayground2.playground/Contents.swift
+++ Playground/MyPlayground2.playground/Contents.swift
@@ -2,27 +2,55 @@ import Foundation
 
 //옵셔널로 선언된 프로퍼티는 nil을 가질 수 있음. -> 나중에 초기화해도됨
 //nil을 담을 수 없는 프로퍼티는 값이 무조건 있어야한다. -> 초기화가 필요함. -> 이니셜라이저 사용
-class Monster {
-    var name: String
-    var level: Int
-    
-    init(name: String, level: Int) {
-        self.name = name
-        self.level = level
-    }
+//class Monster {
+//    var name: String
+//    var level: Int
+//
+//    init(name: String, level: Int) {
+//        self.name = name
+//        self.level = level
+//    }
+//}
+//
+//let easy = Monster(name: "MosonLee", level: 12)
+//
+//var hard = easy
+//
+//print(easy.name, easy.level)
+//print(hard.name, hard.level)
+//
+//
+//struct StructMonster {
+//    var name: String
+//    var level: Int
+//}
+//
+//let structMonster = StructMonster(name: "MosonLEEEEEE", level: 1222222)
+//
+
+//var nickName: String? = "MosonLee"
+//
+//if let name = nickName {
+//    print("닉네임은 \(name)입니다.")
+//} else {
+//    print("없는 값입니다.")
+//}
+
+
+
+//func ex() {
+//    let nickName: String? = "MosonLee"
+//
+//    guard let name = nickName else {
+//        return print("없는 값입니다.")
+//    }
+//    print(name)
+//}
+//ex()
+
+class EX {
+    //저장 프로퍼티(상수, 변수), 인스턴스 프로퍼티
+    var nickName = "MosonLEE"
+    // 저장 프로퍼티(상수, 변수), 타입 프로퍼티
+    static var staticName = "MosonLeeStatic"
 }
-
-let easy = Monster(name: "MosonLee", level: 12)
-
-var hard = easy
-
-print(easy.name, easy.level)
-print(hard.name, hard.level)
-
-
-struct StructMonster {
-    var name: String
-    var level: Int
-}
-
-let structMonster = StructMonster(name: "MosonLEEEEEE", level: 1222222)
--- Playground/SesacWeek3.playground/Contents.swift
+++ Playground/SesacWeek3.playground/Contents.swift
@@ -204,21 +204,32 @@ print(user4.nickname, User.originalName)
  연산 프로퍼티(인스턴스 연산 프로퍼티 / 타입 연산 프로퍼티)
  */
 
+
+// 연산 프로퍼티 활용
+// 저장 프로퍼티는 메모리에 올라간다., 연산 프로퍼티는 저장 프로퍼티를 활용해서 원하는 값을 반환하는 용도로 주로 사용
+// 읽기 전용(read-only) 프로퍼티이지만 계산하는 값에 따라 결과가 달라질 수 있기 때문이다.
 struct BMI {
-    var nickname: String
+    var nickname: String {
+        willSet {
+            print("유저 닉네임이 \(nickname)에서 \(newValue)로 변경될 예정입니다.")
+        }
+        didSet {
+            print("유저 닉네임 변경 완료!!! \(oldValue) -> \(nickname)으로 바뀜!!")
+        }
+    }
     var weight: Double
     var height: Double
-    
-    // 연산 프로퍼티 활용
-    // 저장 프로퍼티는 메모리 O, 연산 프로퍼티는 저장 프로퍼티를 활용해서 원하는 값을 반환하는 용도로 주로 사용!
-    // 읽기 전용(read-only) 프로퍼티이지만 계산하는 값에 따라 결과가 달라질 수 있기 때문!
+
     var BMIResult: String {
         get {
             let bmiValue = (weight * weight) / height
             let bmiStatus = bmiValue < 18.5 ? "저체중" : "정상 이상"
             return "\(nickname)님의 BMI 지수는 \(bmiValue)로 \(bmiStatus)입니다!"
         }
+        
+        // set은 옵션
         set {
+            
             nickname = newValue
         }
     }
@@ -242,11 +253,32 @@ print(result)
 bmi.BMIResult = "올라프" // set
 print(bmi.BMIResult)
 
-class FoodRestaurant {
-    let name = "잭치킨"
-    var totalOrderCount = 1 // 총 주문건수
+//class FoodRestaurant {
+//    let name = "잭치킨"
+//    var totalOrderCount = 1 // 총 주문건수
+//
+//    var nowOrder: Int {
+//        get {
+//            return totalOrderCount * 5000
+//        }
+//        set { // set은 옵션
+//            totalOrderCount += newValue // 기본 파라미터 newValue, 변경 가능!
+//        }
+//    }
+//}
+
+class TypeFoodRestaurant {
+    static let name = "잭치킨" //타입 상수 저장 프로퍼티
+    static var totalOrderCount = 10 {
+        willSet { //변경 되기 직전에 실행
+            print("총 주문건 수가 \(totalOrderCount)에서 \(newValue)로 변경될 예정입니다.")
+        }
+        didSet { // 변경 되고 난 직후에 실행
+            print("총 주문건 수가 \(oldValue)에서 \(totalOrderCount)로 변경될 예정입니다.")
+        }
+    } // 총 주문건수 // 타입 변수 저장 프로퍼티
     
-    var nowOrder: Int {
+    static var nowOrder: Int {
         get {
             return totalOrderCount * 5000
         }
@@ -255,43 +287,75 @@ class FoodRestaurant {
         }
     }
 }
+TypeFoodRestaurant.nowOrder // 타입 연산 프로퍼티 get
+TypeFoodRestaurant.nowOrder = 15 // 타입 연산 프로퍼티 Set
+//TypeFoodRestaurant.nowOrder
 
-let food = FoodRestaurant()
+//Property Observer: 저장 프로퍼티에서 주로 사용되고, 저장 프로퍼티 값을 관찬을 하다가 변경이 될 것 같을 때 or 변경이 되었을 때 호출됨 (willSet / didSet)
 
-print(food.nowOrder)
+//
+//let food = FoodRestaurant()
+//
+//print(food.nowOrder)
+//
+//food.totalOrderCount += 5
+//food.totalOrderCount += 20
+//food.totalOrderCount += 100
+//
+//food.nowOrder = 5
+//food.nowOrder = 20
+//food.nowOrder = 100
+//// 연산 프로퍼티의 nowOrder에 값을 넘겨 최종적으로 totalOrderCount를 업데이트한다
+//
+//print(food.nowOrder)
+//// 그리고 위에서 set으로 업데이트한 값을 이용하여 get으로 최종 결과를 반환
+//
+//
+//// 열거형은 타입 자체 => 인스턴스 생성 불가능 => 초기화 구문 X
+//// 인스턴스 생성을 통해서 접근할 수 있는 인스턴스 저장 프로퍼티 사용 불가! 인스턴스 연산 프로퍼티는?
+//// 메모리의 관점 + 열거형이 컴파일 타임에 확정되어야 한다! => 인스턴스 연산 프로퍼티는 열거형에서 사용할 수 있다...
+//// 타입 저장 프로퍼티 => 열거형에서 사용 가능!
+//enum ViewType {
+//    case start
+//    case chang
+//
+//    // var nickname: String = 고래밥
+//
+//    var nickname: String {
+//        return "aa"
+//    }
+//
+//    static var title = "시작하기"
+//}
+//// 타입 저장 프로퍼티는 인스턴스랑 상관없이 접근 가능! 따라서 열거형에서 타입 저장 프로퍼티, 타입 연산 프로퍼티는 모두 사용 가능!
+//// 인스턴스 저장 프로퍼티는 메모리, 값이 달라질 수 있음 => X => 열거형은 초기화 구문을 만들 수 없기 때문이다.
+//// 타입 저장 프로퍼티, 타입 연산 프로퍼티, 인스턴스 저장 프로퍼티, 인스턴스 연산 프로퍼티
+//ViewType.title
+//
 
-food.totalOrderCount += 5
-food.totalOrderCount += 20
-food.totalOrderCount += 100
 
-food.nowOrder = 5
-food.nowOrder = 20
-food.nowOrder = 100
-// 연산 프로퍼티의 nowOrder에 값을 넘겨 최종적으로 totalOrderCount를 업데이트한다
+//메서드: 타입 메서드 & 인스턴스 메서드
 
-print(food.nowOrder)
-// 그리고 위에서 set으로 업데이트한 값을 이용하여 get으로 최종 결과를 반환
-
-
-// 열거형은 타입 자체 => 인스턴스 생성 불가능 => 초기화 구문 X
-// 인스턴스 생성을 통해서 접근할 수 있는 인스턴스 저장 프로퍼티 사용 불가! 인스턴스 연산 프로퍼티는?
-// 메모리의 관점 + 열거형이 컴파일 타임에 확정되어야 한다! => 인스턴스 연산 프로퍼티는 열거형에서 사용할 수 있다...
-// 타입 저장 프로퍼티 => 열거형에서 사용 가능!
-enum ViewType {
-    case start
-    case chang
+struct Coffee {
+    static var name = "아메리카노"
+    static var shot = 2
+    var price = 4900
     
-    // var nickname: String = 고래밥
-    
-    var nickname: String {
-        return "aa"
+    mutating func plusShot() {
+        Coffee.shot += 1
+        price += 300
     }
-    
-    static var title = "시작하기"
+    static func minusShot() {
+        shot -= 1
+    }
 }
-// 타입 저장 프로퍼티는 인스턴스랑 상관없이 접근 가능! 따라서 열거형에서 타입 저장 프로퍼티, 타입 연산 프로퍼티는 모두 사용 가능!
-// 인스턴스 저장 프로퍼티는 메모리, 값이 달라질 수 있음 => X => 열거형은 초기화 구문을 만들 수 없기 때문이다.
-// 타입 저장 프로퍼티, 타입 연산 프로퍼티, 인스턴스 저장 프로퍼티, 인스턴스 연산 프로퍼티
-ViewType.title
+//
+//class Latte: Coffee {
+//    override class func minusShot() { //슈퍼클래스 타입 메서드를 재정의해서 쓰고 싶다면 class!
+//
+//        }
+//    }
 
-//: [Next](@next)
+
+////: [Next](@next)
+
