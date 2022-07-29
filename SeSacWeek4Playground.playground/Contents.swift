096fe68fc70eabf54b828cfe23192430ca86143f
이승후 <ron222@naver.com>
2022-07-29 12:22:19 +0900

    refacotr: code

--- SeSacWeek4Playground.playground/Contents.swift
+++ SeSacWeek4Playground.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 struct ExchangeRate {
-    
     var currencyRate: Double {
         willSet {
             print("willSet: 환율이 \(currencyRate)에서 \(newValue)로 변경될 예정입니다..")
@@ -12,26 +11,26 @@ struct ExchangeRate {
     }
     
     var USD: Double {
-        get {
-            print("현재 ")
-            return KRW / currencyRate
+        willSet {
+            print("USD willSet - 환전 금액: USD \(KRW / currencyRate)")
         }
-        set {
-            currencyRate = newValue
+        didSet {
+            print("USD didSet - KRW: \(KRW)원 -> \(USD)")
         }
     }
     
     var KRW: Double {
         get {
-            print("원화로는 \(currencyRate * USD)입니다.")
             return currencyRate * USD
         }
         set {
-            print(newValue)
+            USD = newValue / currencyRate
         }
     }
 }
 
-var rate = ExchangeRate(currencyRate: 1000)
-rate.KRW = 5000
-rate.USD = 1000
+var rate = ExchangeRate(currencyRate: 1100, USD: 0)
+
+rate.KRW = 500000
+rate.currencyRate = 1350
+rate.KRW = 500000
