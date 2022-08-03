7f0be4774d5a3468982c533b53e2d7bbdb1aef4a
이승후 <ron222@naver.com>
2022-08-03 22:32:41 +0900

    add: playground

---
+++ Playground/SesacNetworkPlayground.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+//일급 객체, 클로저
+//일급 객체: 변수나 상수에 함수를 넣을 수 있다, 반환타입에 함수를 넣을 수 있다, 인자 값으로 함수를 넣을 수 있다.
+
+/*
+ 1급 객체:
+ 1. 변수나 상수에 함수를 대입할 수 있다.
+
+ */
+
+func checkBankInformation(bank: String) -> Bool {
+    let bankArray = ["우리", "국민", "신한"]
+    return bankArray.contains(bank) ? true : false
+}
+
+//변수나 상수에 함수를 실행해서 반환된 반환값을 대입한 것 (1급 객체의 특성은 아님)
+let checkResult = checkBankInformation(bank: "우리")
+print(checkResult)
+
+//변수나 상수에 함수 자체를 대입할 수 있다(1급 객체의 특성)
+//단지 함수만 대입한 것으로, 실행된 상태는 아님.
+let checkAccount = checkBankInformation
+
+//(Stirng) -> Bool 이게 뭘까? -> Function Type (ex. Tuple)
+
+//함수를 호출해줘야 실행이 된다.
+checkAccount("신한")
+
+//Function Type: (String) -> String
+func hello(userName: String) -> String {
+    return "저는 \(userName)입니다."
+}
+
+//Function Type: (String, Int) -> String
+func hello(userName: String, age: Int) -> String {
+    return "저는 \(userName), \(age)살입니다."
+}
+
+//오버로딩 특성으로 함수를 구분하기 힘들 때, 타입 어노테이션을 통해서 함수를 구별할 수 이싿.
+//하지만 타입 어노테이션만으로 함수를 구별할 수 없는 상황도 있다./
+//함수 표기법을 사용한다면 타입 어노테이션을 생랼가허다로 함수를 구별할 수 있다.
+
+let result: (String) -> String = hello(userName: ) // 함수 표기법
+result("고래밥")
+
+let ageResult: (String, Int) -> String = hello
+ageResult("고래밥", 30)
+
+func hello(nickname: String) -> String {
+    return "저는 \(nickname)입니다."
+}
+let result2 = hello(nickname: ) // 함수 표기법
+result2("상어밥")
+
+//2번 특성
+func oddNumber() {
+    print("홀수")
+}
+
+func evenNumber() {
+    print("짝수")
+}
+
+func resultNumber(number: Int, odd: () -> (), even: () -> () ) {
+    return number.isMultiple(of: 2) ? even() : odd()
+}
+resultNumber(number: 9, odd: oddNumber, even: evenNumber)
+resultNumber(number: <#T##Int#>) {
+    <#code#>
+} even: {
+    <#code#>
+}
+
+
