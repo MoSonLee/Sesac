48871e4e3c831c3ae11ad412d1365ba591804660
이승후 <ron222@naver.com>
2022-08-08 09:32:37 +0900

    add: Sesacweek6

---
+++ SeSacWekk6/SeSacWekk6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacWekk6
+//
+//  Created by 이승후 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
