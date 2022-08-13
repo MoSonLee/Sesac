1c4a83486486e3d82e97ee7159181ae9a016e24e
이승후 <ron222@naver.com>
2022-08-13 19:53:07 +0900

    feat: make toast and search similar celebrity logic

--- SeSacWekk6/SeSacWekk6/ViewContoller/CammeraViewController.swift
+++ SeSacWekk6/SeSacWekk6/ViewContoller/CammeraViewController.swift
@@ -11,25 +11,30 @@ import YPImagePicker
 import Alamofire
 import SwiftyJSON
 
-class CammeraViewController: UIViewController {
+import Toast
+
+final class CammeraViewController: UIViewController {
     
-    @IBOutlet weak var ypImagePickerButton: UIButton!
-    @IBOutlet weak var cameraButton: UIButton!
-    @IBOutlet weak var photoLibraryButton: UIButton!
-    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet private weak var ypImagePickerButton: UIButton!
+    @IBOutlet private weak var cameraButton: UIButton!
+    @IBOutlet private weak var photoLibraryButton: UIButton!
+    @IBOutlet private weak var resultImageView: UIImageView!
+    @IBOutlet weak var saveToGalleryButton: UIButton!
+    @IBOutlet weak var cloverFaceButton: UIButton!
     
-    let picker = UIImagePickerController()
+    private let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        saveToGalleryButton.setTitle("갤러리에 저장", for: .normal)
+        cloverFaceButton.setTitle("닮은 연예인 찾기", for: .normal)
         ypImagePickerButton.setTitle("IMAGE PICKER", for: .application)
         cameraButton.setTitle("Camera", for: .normal)
         photoLibraryButton.setTitle("Photo", for: .normal)
-        
         picker.delegate = self
     }
     
-    @IBAction func ypImageButtonClicked(_ sender: UIButton) {
+    @IBAction private func ypImageButtonClicked(_ sender: UIButton) {
         let picker = YPImagePicker()
         picker.didFinishPicking { [unowned picker] items, _ in
             if let photo = items.singlePhoto {
@@ -38,7 +43,6 @@ class CammeraViewController: UIViewController {
                 print(photo.originalImage)
                 //                print(photo.modifiedImage)
                 //                print(photo.exifMeta)
-                
                 self.resultImageView.image = photo.image
             }
             picker.dismiss(animated: true, completion: nil)
@@ -46,7 +50,7 @@ class CammeraViewController: UIViewController {
         present(picker, animated: true, completion: nil)
     }
     
-    @IBAction func cameraButtonClicked(_ sender: UIButton) {
+    @IBAction private func cameraButtonClicked(_ sender: UIButton) {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             print("사용불가 + 알럿")
             return
@@ -56,7 +60,7 @@ class CammeraViewController: UIViewController {
         present(picker, animated: true)
     }
     
-    @IBAction func photoLibraryButtonClicked(_ sender: UIButton) {
+    @IBAction private func photoLibraryButtonClicked(_ sender: UIButton) {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             print("사용불가 + 알럿")
             return
@@ -66,34 +70,33 @@ class CammeraViewController: UIViewController {
         present(picker, animated: true)
     }
     
-    @IBAction func saveToPhotoLibrary(_ sender: UIButton) {
-        
+    @IBAction private func saveToPhotoLibrary(_ sender: UIButton) {
         if let image = resultImageView.image {
             UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            self.view.makeToast("사진 저장이 완료되었습니다!")
         }
     }
-    //이미지뷰 이미지 > 네이버 > 얼굴분석 해줘 요청 > 응다ㅣㅂ
+    //이미지뷰 이미지 > 네이버 > 얼굴분석 해줘 요청 > 응답
     //문자열이 아닌 파일, 이미지, pdf 파일 자체가 그대로 전송 되지 않음. -> 텍스트 형태로 인코딩
     //어떤 파일의 종류가 명시되는지 -> Content Type
-    @IBAction func clovaFaceButtonClicked(_ sender: UIButton) {
+    @IBAction private func clovaFaceButtonClicked(_ sender: UIButton) {
         let url = "https://openapi.naver.com/v1/vision/celebrity"
         let header: HTTPHeaders = [
             "X-Naver-Client-Id": "tSUzT1r164LmbgFXn59B",
             "X-Naver-Client-Secret": "rtxl2UV_IV",
-//            "Content-Type": "multipart/form-data"
+            "Content-Type": "multipart/form-data"
         ]
-        
         //UIImage를 텍스트 형태(바이너리 타입)로 변환해서 전달
-        guard let imageData = resultImageView.image?.jpegData(compressionQuality: 0.4) else {
-            return
-        }
+        let imageData = resultImageView.image?.jpegData(compressionQuality: 0.4)
         
         AF.upload(multipartFormData: { multipartFormData in
-            multipartFormData.append(Data("one".utf8), withName: "image")
+            multipartFormData.append(imageData!, withName: "image")
         }, to: url, headers: header).validate(statusCode: 200...500).responseData { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
+                let celebrity = json["faces"][0]["celebrity"]["value"]
+                self.view.makeToast("나와 닮은 연예인은\(celebrity)입니다.")
                 print(json)
                 
             case .failure(let error):
@@ -104,7 +107,6 @@ class CammeraViewController: UIViewController {
 }
 
 extension CammeraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     //사진을 선택하거나 카메라 촬영 직푸
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         print(#function)
@@ -114,7 +116,6 @@ extension CammeraViewController: UIImagePickerControllerDelegate, UINavigationCo
             dismiss(animated: true)
         }
     }
-    
     //취소 버튼 클릭시
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         print(#function)
