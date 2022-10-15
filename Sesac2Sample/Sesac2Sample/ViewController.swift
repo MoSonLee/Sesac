5de0f04e5e5451e511f201c67d01bc75211765bd
MosonLee <ronlee6235@gmail.com>
2022-10-15 22:13:00 +0900

    feat: sesac2 Sample

---
+++ Sesac2Sample/Sesac2Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sesac2Sample
+//
+//  Created by 이승후 on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
