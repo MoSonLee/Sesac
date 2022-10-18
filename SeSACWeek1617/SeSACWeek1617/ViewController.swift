a5c5e4895d85fc1b2065f9c1b1e44a082124a404
MosonLee <ron222@naver.com>
2022-10-18 09:54:52 +0900

    add: SeSACWekk1617 project

---
+++ SeSACWeek1617/SeSACWeek1617/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek1617
+//
+//  Created by 이승후 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
