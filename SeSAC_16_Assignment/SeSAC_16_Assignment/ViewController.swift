37a0b342ab18d4d8bffb1553edcfcabf2c027e72
MosonLee <ron222@naver.com>
2022-10-19 21:38:50 +0900

    add: SeSAC_16_Assignment project

---
+++ SeSAC_16_Assignment/SeSAC_16_Assignment/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SeSAC_16_Assignment
+//
+//  Created by 이승후 on 2022/10/19.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    let searchBar = UISearchBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setComponents()
+        setConstraints()
+    }
+    
+    private func setComponents() {
+        
+    }
+    
+    private func setConstraints() {
+        view.backgroundColor = .lightGray
+    }
+}
