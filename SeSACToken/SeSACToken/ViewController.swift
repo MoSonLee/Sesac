2575f478a2d5b8a54f94dd5f43af2a10f69266fd
MosonLee <ron222@naver.com>
2022-11-02 11:30:49 +0900

    add: SeSACToken project

---
+++ SeSACToken/SeSACToken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACToken
+//
+//  Created by 이승후 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
