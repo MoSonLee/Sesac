7c7f57d3c383b9a6f342128e2cd83cbb28cc0cdf
MosonLee <ron222@naver.com>
2022-11-22 11:39:47 +0900

    feat: Chat project

---
+++ SeSAC2Chat/SeSAC2Chat/APIKey.swift
@@ -0,0 +1,15 @@
+//
+//  APIKey.swift
+//  SeSAC2Chat
+//
+//  Created by jack on 2022/11/22.
+//
+
+import Foundation
+
+enum APIKey {
+    static let url = "http://api.sesac.co.kr:2022/chats"
+    static let socket = "http://api.sesac.co.kr:2022"
+    static let header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2MyYjgyN2JhZDY1M2FjZDMzZmIwMCIsImlhdCI6MTY2OTA4MTk4NiwiZXhwIjoxNjY5MTY4Mzg2fQ.neTKy3Li6ApPs2O5j7twTEpRHiHg7GvKj3GB6kDybws"
+    static let userId = "637c2be07bad653acd33fb46"
+}
